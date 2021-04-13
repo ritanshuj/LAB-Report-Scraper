680bd90e273e75d32d3c0dad2b1e15f511345898
ritanshu jayant <67803038+ritanshuj@users.noreply.github.com>
2021-04-13 16:57:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,16 +76,17 @@ def scrape():
         
       
     
-    else :'''
+    else :
     uploaded_file=request.files.getlist("image/PDF")
 
 
-    text=[]
+    
     for filename in uploaded_file:
 
         text.append(str(((pytesseract.image_to_string(Image.open(filename))))).lower())
            
-   
+   '''
+    text=['hb 67','tlc 9.0']
 
     result={}
     p = re.compile('\d+(\.\d+)?')
